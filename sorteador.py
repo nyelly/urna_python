73486ef02726a73b486ea23f3e7724065bb2a227
Flavinho do grau <nyelly437@gmail.com>
2022-12-09 22:13:47 -0300

    Primeiro commit

---
+++ sorteador.py
@@ -0,0 +1,6 @@
+from random import randint
+
+def sorteador_numeros():
+    Sorteador = [randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000), randint(0, 10000)]
+    print(Sorteador)
+    return Sorteador
---
+++ urna.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import messagebox
+
+from sorteador import sorteador_numeros
+
+class Aplication():
+    def __init__(self, janela):
+        janela.title("Aplicação tkinter")
+        janela.geometry("300x300")
+        self.container1 = Frame(janela)
+        self.container1.grid(row=0, column=0)
+        self.container2 = Frame(janela)
+        self.container2.grid(row=0, column=1)
+        self.container3 = Frame(janela)
+        self.container3.grid(row=1, column=0)
+        self.container4 = Frame(janela)
+        self.container4.grid(row=1, column=0)
+
+        self.label1 = Label(self.container1, text="Sorteador")        
+        self.label1.pack()
+
+        self.label2 = Label(self.container1, text="")
+        self.label2.pack()
+       
+        self.btn1 = Button(self.container3, text= "Sorteador" ,command=self.mudaNumero)
+        self.btn1.pack()
+
+        self.btn2 = Button(self.container3, text= "Finalizar" ,command=janela.destroy)
+        self.btn2.pack()
+
+    def mudaNumero(self):
+        
+        numeros_retornados = sorteador_numeros()
+        self.label2["text"]= numeros_retornados
+    
+
+ 
+app = Tk()
+Aplication(app)
+app.mainloop()
